92a46a7ab4b65bc26c2cb5d46523ba7608a8f258
ChairawichZ6 <70460005+ChairawichZ6@users.noreply.github.com>
2023-07-18 21:35:29 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
-import React, { useState } from 'react';
-import { View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Timer from './src/Timer';
-import PictureProcess from './src/PictureProcess';
-import NavBar from './src/NavBar';
-import AppBar from './src/AppBar';
+import React, { useState } from "react";
+import { View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
+import Timer from "./src/Timer";
+import PictureProcess from "./src/PictureProcess";
+import Task from "./src/Task"; // Import the Task component
+import NavBar from "./src/NavBar";
+import AppBar from "./src/AppBar";
 
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
 const TimerScreen = ({ onReset }) => (
   <View style={{ flex: 1 }}>
@@ -23,24 +26,40 @@ const ResultScreen = ({ onReset }) => (
   </View>
 );
 
-const App = () => {
-  const [activeScreen, setActiveScreen] = useState('timer');
+const ButtomTab = () => {
+  const [activeScreen, setActiveScreen] = useState("timer");
   const [resetPicture, setResetPicture] = useState(false);
 
   const handleResetPicture = () => {
     setResetPicture(true);
   };
+  return (
+    <Tab.Navigator
+      tabBar={(props) => (
+        <NavBar
+          {...props}
+          activeScreen={activeScreen}
+          setActiveScreen={setActiveScreen}
+        />
+      )}
+    >
+      <Tab.Screen name="Timer">
+        {() => <TimerScreen onReset={handleResetPicture} />}
+      </Tab.Screen>
+      <Tab.Screen name="Result">
+        {() => <ResultScreen onReset={handleResetPicture} />}
+      </Tab.Screen>
+    </Tab.Navigator>
+  );
+};
 
+const App = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator tabBar={(props) => <NavBar {...props} activeScreen={activeScreen} setActiveScreen={setActiveScreen} />}>
-        <Tab.Screen name="Timer">
-          {() => <TimerScreen onReset={handleResetPicture} />}
-        </Tab.Screen>
-        <Tab.Screen name="Result">
-          {() => <ResultScreen onReset={handleResetPicture} />}
-        </Tab.Screen>
-      </Tab.Navigator>
+      <Stack.Navigator>
+        <Stack.Screen name="Timer" component={ButtomTab} options={{headerShown : false}} />
+        <Stack.Screen name="Task" component={Task} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };
